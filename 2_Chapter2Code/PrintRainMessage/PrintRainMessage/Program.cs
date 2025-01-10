bb42e067950f7907f3d1af77f0253bbaffb24959
PovilasMat <povilas.matusevicius@gmail.com>
2025-01-10 21:23:07 +0100

    #2 Chapter2Code from website

---
+++ 2_Chapter2Code/PrintRainMessage/PrintRainMessage/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace PrintRainMessage
+{
+    /// <summary>
+    /// Prints a rain message
+    /// </summary>
+
+    internal class Program
+    {
+        /// <summary>
+        /// Prints a rain message
+        /// </summary>
+        /// <param name="args">command-line args</param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, world");
+            Console.WriteLine("Chinese Democracy is done and it's November");
+            Console.WriteLine("Is it raining?");
+            Console.WriteLine();
+        }
+    }
+}
---
+++ 2_Chapter2Code/UnityPrintRainMessage/Assets/Scripts/PrintRainMessage.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Prints a rain message
+/// </summary>
+public class PrintRainMessage : MonoBehaviour
+{
+    /// <summary>
+	/// Start is called before the first frame update
+	/// </summary>	
+    void Start()
+    {
+        Debug.Log("Hello, world");
+        Debug.Log("Chinese Democracy is done and it's November");
+        Debug.Log("Is it raining?");
+    }
+}
+
