e36cb792c21a3eeb92e8d3a991ea696ca6b54aa6
PovilasMat <povilas.matusevicius@gmail.com>
2025-01-13 22:59:23 +0100

    More C#

---
+++ 20_If_Statements/20_If_Statements/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _20_If_Statements
+{
+    /// <summary>
+    /// Demonstrates various forms of if statements.
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // prompt for and get user input
+            Console.Write("Pick up the shiny thing? (1 for yes, 2 for no: ");
+            int answer = int.Parse(Console.ReadLine());
+
+            // print appropriate statement
+            if ( answer == 1 )
+            {
+                Console.WriteLine("You have the shiny thing");
+            }
+            else if (answer == 2)
+            {
+                Console.WriteLine("You do not have the shiny thing");
+            }
+            else
+            {
+                Console.WriteLine("Invalid input, dork!");
+            }
+        }
+    }
+}
