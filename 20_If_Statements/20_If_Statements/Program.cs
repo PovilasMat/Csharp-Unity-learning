e099edea5a498cd93b5e3b4d6a31f92e50f4996b
PovilasMat <povilas.matusevicius@gmail.com>
2025-01-14 08:00:48 +0100

    Module 5 Exercise 1+2

--- 20_If_Statements/20_If_Statements/Program.cs
+++ 20_If_Statements/20_If_Statements/Program.cs
@@ -8,21 +8,35 @@
         static void Main(string[] args)
         {
             // prompt for and get user input
-            Console.Write("Pick up the shiny thing? (1 for yes, 2 for no: ");
+            Console.Write("Pick up the shiny thing? (1 for yes, 2 for no): ");
             int answer = int.Parse(Console.ReadLine());
 
-            // print appropriate statement
-            if ( answer == 1 )
+            // print appropriate statement with if statement
+            //if ( answer == 1 )
+            //{
+            //    Console.WriteLine("You have the shiny thing");
+            //}
+            //else if (answer == 2)
+            //{
+            //    Console.WriteLine("You do not have the shiny thing");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Invalid input, dork!");
+            //}
+
+            // print appropriate statement with switch statement
+            switch (answer)
             {
-                Console.WriteLine("You have the shiny thing");
-            }
-            else if (answer == 2)
-            {
-                Console.WriteLine("You do not have the shiny thing");
-            }
-            else
-            {
-                Console.WriteLine("Invalid input, dork!");
+                case 1:
+                    Console.WriteLine("You have the shiny thing");
+                    break;
+                case 2:
+                    Console.WriteLine("You do not have the shiny thing");
+                    break;
+                default:
+                    Console.WriteLine("Invalid input, dork!");
+                    break;
             }
         }
     }
---
+++ 22_Exercise2/22_Exercise2/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace _22_Exercise2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DisplayMenu();
+            int choice = GetUserChoice();
+            ExecuteChoice(choice);
+        }
+
+        static void DisplayMenu()
+        {
+            Console.WriteLine("**************");
+            Console.WriteLine("Menu");
+            Console.WriteLine("1 - New Game");
+            Console.WriteLine("2 - Load Game");
+            Console.WriteLine("3 - Options");
+            Console.WriteLine("4 - Quit");
+            Console.WriteLine("**************");
+        }
+
+        static int GetUserChoice()
+        {
+            int choice;
+            while (!int.TryParse(Console.ReadLine(), out choice) || choice < 1 || choice > 4)
+            {
+                Console.WriteLine("Invalid choice, please enter a number between 1 and 4.");
+            }
+            return choice;
+        }
+
+        static void ExecuteChoice(int choice)
+        {
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine("Starting a new game...");
+                    break;
+                case 2:
+                    Console.WriteLine("Loading game...");
+                    break;
+                case 3:
+                    Console.WriteLine("Options menu...");
+                    break;
+                case 4:
+                    Console.WriteLine("Quitting game...");
+                    break;
+            }
+        }
+    }
+}
