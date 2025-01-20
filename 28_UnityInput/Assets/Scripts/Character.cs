4a942a20eb255b8bc9642b68a07bf7a935a1ffec
PovilasMat <povilas.matusevicius@gmail.com>
2025-01-20 23:03:16 +0100

    Mouse position processing and clamping

---
+++ 28_UnityInput/Assets/Scripts/Character.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Character : MonoBehaviour
+{
+    float colliderHalfWidth;
+    float colliderHalfHeight;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        BoxCollider2D collider = GetComponent<BoxCollider2D>();
+        colliderHalfWidth = collider.size.x / 2;
+        colliderHalfHeight = collider.size.y / 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // convert mouse position to world position
+        Vector3 position = Input.mousePosition;
+        position.z = -Camera.main.transform.position.z;
+        position = Camera.main.ScreenToWorldPoint(position);
+
+        // move character
+        transform.position = position;
+        ClampInScreen();
+    }
+
+    /// Clamps the character in the screen
+    void ClampInScreen()
+    {
+        Vector3 position = transform.position;
+
+        // clam horinzontally
+        if (position.x - colliderHalfWidth < ScreenUtils.ScreenLeft)
+        {
+            position.x = ScreenUtils.ScreenLeft + colliderHalfWidth;
+        }
+        else if (position.x + colliderHalfWidth > ScreenUtils.ScreenRight)
+        {
+            position.x = ScreenUtils.ScreenRight - colliderHalfWidth;
+        }
+
+        // clamp vertically
+        if (position.y + colliderHalfHeight > ScreenUtils.ScreenTop)
+        {
+            position.y = ScreenUtils.ScreenTop - colliderHalfHeight;
+        }
+        else if (position.y - colliderHalfHeight < ScreenUtils.ScreenBottom)
+        {
+            position.y = ScreenUtils.ScreenBottom + colliderHalfHeight;
+        }
+
+        // set the new position of the character
+        transform.position = position;
+    }
+}
