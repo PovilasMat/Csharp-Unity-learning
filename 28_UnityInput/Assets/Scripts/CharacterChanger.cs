be0f85732cdf21d8beab5f18f4820ff490a8c61a
PovilasMat <povilas.matusevicius@gmail.com>
2025-01-21 22:36:19 +0100

    Mouse clicks

---
+++ 28_UnityInput/Assets/Scripts/CharacterChanger.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class CharacterChanger : MonoBehaviour
+{
+    [SerializeField] GameObject characterPrefab0;
+    [SerializeField] GameObject characterPrefab1;
+    [SerializeField] GameObject characterPrefab2;
+    [SerializeField] GameObject characterPrefab3;
+
+    // need for location of new character
+    GameObject currentCharacter;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        currentCharacter = Instantiate<GameObject>(characterPrefab0, Vector3.zero, Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // change character on mouse press
+        if (Input.GetMouseButtonDown(0))
+        {
+            // save position and destroy current character
+            Vector3 position = currentCharacter.transform.position;
+            Destroy(currentCharacter);
+            
+            int prefabNumber = Random.Range(0, 4);
+            switch (prefabNumber)
+            {
+                case 0:
+                    currentCharacter = Instantiate<GameObject>(characterPrefab0, position, Quaternion.identity);
+                    break;
+                case 1:
+                    currentCharacter = Instantiate<GameObject>(characterPrefab1, position, Quaternion.identity);
+                    break;
+                case 2:
+                    currentCharacter = Instantiate<GameObject>(characterPrefab2, position, Quaternion.identity);
+                    break;
+                case 3:
+                    currentCharacter = Instantiate<GameObject>(characterPrefab3, position, Quaternion.identity);
+                    break;
+            }
+        }
+    }
+}
