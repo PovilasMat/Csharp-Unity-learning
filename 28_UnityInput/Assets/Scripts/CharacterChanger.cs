3a0f901a654b7d9722eec586c3b5a02553c6a359
PovilasMat <povilas.matusevicius@gmail.com>
2025-01-21 23:31:13 +0100

    Using InputManager instead of just click

--- 28_UnityInput/Assets/Scripts/CharacterChanger.cs
+++ 28_UnityInput/Assets/Scripts/CharacterChanger.cs
@@ -10,6 +10,9 @@ public class CharacterChanger : MonoBehaviour
     // need for location of new character
     GameObject currentCharacter;
 
+    // first frame input support
+    bool previousChangeCharacterInput = false;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -20,28 +23,40 @@ public class CharacterChanger : MonoBehaviour
     void Update()
     {
         // change character on mouse press
-        if (Input.GetMouseButtonDown(0))
+        // if (Input.GetMouseButtonDown(0))
+        if (Input.GetAxis("Change Character") > 0)
         {
-            // save position and destroy current character
-            Vector3 position = currentCharacter.transform.position;
-            Destroy(currentCharacter);
-            
-            int prefabNumber = Random.Range(0, 4);
-            switch (prefabNumber)
+            // change character input
+            if (!previousChangeCharacterInput)
             {
-                case 0:
-                    currentCharacter = Instantiate<GameObject>(characterPrefab0, position, Quaternion.identity);
-                    break;
-                case 1:
-                    currentCharacter = Instantiate<GameObject>(characterPrefab1, position, Quaternion.identity);
-                    break;
-                case 2:
-                    currentCharacter = Instantiate<GameObject>(characterPrefab2, position, Quaternion.identity);
-                    break;
-                case 3:
-                    currentCharacter = Instantiate<GameObject>(characterPrefab3, position, Quaternion.identity);
-                    break;
-            }
+                previousChangeCharacterInput = true;
+            
+                // save position and destroy current character
+                Vector3 position = currentCharacter.transform.position;
+                Destroy(currentCharacter);
+            
+                int prefabNumber = Random.Range(0, 4);
+                switch (prefabNumber)
+                {
+                    case 0:
+                        currentCharacter = Instantiate<GameObject>(characterPrefab0, position, Quaternion.identity);
+                        break;
+                    case 1:
+                        currentCharacter = Instantiate<GameObject>(characterPrefab1, position, Quaternion.identity);
+                        break;
+                    case 2:
+                        currentCharacter = Instantiate<GameObject>(characterPrefab2, position, Quaternion.identity);
+                        break;
+                    case 3:
+                        currentCharacter = Instantiate<GameObject>(characterPrefab3, position, Quaternion.identity);
+                        break;
+                }
+            } 
+        }
+        else
+        {
+            // no change character input
+            previousChangeCharacterInput = false;   
         }
     }
 }
