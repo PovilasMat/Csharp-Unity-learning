1f18e9e225e4ec52c4bb5ec2d4a989fe89bd6f13
PovilasMat <povilas.matusevicius@gmail.com>
2025-01-23 23:35:45 +0100

    exercise6 solved

--- 29_Exercise6/Assets/Scripts/MouseButtonProcessor.cs
+++ 29_Exercise6/Assets/Scripts/MouseButtonProcessor.cs
@@ -19,11 +19,44 @@ public class MouseButtonProcessor : MonoBehaviour
 	/// <summary>
 	/// Update is called once per frame
 	/// </summary>
-	void Update()
-	{
+	  void Update()
+    {
         // spawn teddy bear as appropriate
+        if (Input.GetAxis("SpawnTeddyBear") > 0)
+        {
+            if (!spawnInputOnPreviousFrame)
+            {
+                //transform the mouse position to world position
+                Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                mousePosition.z = -Camera.main.transform.position.z; // Ensure the z position is set to 0
+                Instantiate(prefabTeddyBear, mousePosition, Quaternion.identity);
+            }
+            spawnInputOnPreviousFrame = true;
+        }
+        else
+        {
+            spawnInputOnPreviousFrame = false;
+        }
 
         // explode teddy bear as appropriate
-		
-	}
+        if (Input.GetAxis("ExplodeTeddyBear") > 0)
+        {
+            if (!explodeInputOnPreviousFrame)
+            {
+                // find the teddy bear to explode
+                GameObject teddyBear = GameObject.FindWithTag("TeddyBear");
+                if (teddyBear != null)
+                {
+                    // explode the teddy bear
+                    Instantiate(prefabExplosion, teddyBear.transform.position, Quaternion.identity);
+                    Destroy(teddyBear);
+                }
+            }
+            explodeInputOnPreviousFrame = true;
+        }
+        else
+        {
+            explodeInputOnPreviousFrame = false;
+        }
+    }
 }
