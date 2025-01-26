0b0f2732f37186aa4072e191a86871e6be51f7e4
PovilasMat <povilas.matusevicius@gmail.com>
2025-01-26 18:07:41 +0100

    Module3 C# scripts

---
+++ 34_whileLoops/34_whileLoops/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _34_whileLoops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // prompt for and get health
+            Console.WriteLine("Enter your health: ");
+            int health = int.Parse(Console.ReadLine());
+
+            // input validation
+            while (health < 0 || health > 100)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Health must be between 0 and 100. Please try again: ");
+                health = int.Parse(Console.ReadLine());
+            }
+            // print health
+            Console.WriteLine();
+            Console.WriteLine("Your health is " + health);
+
+        }
+    }
+}
---
+++ 35_Exercise10/35_Exercise10/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _35_Exercise10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a menu for a game. The menu should have the following options:
+            // 1 - New Game
+            // 2 - Load Game
+            // 3 - Options
+            // 4 - Quit
+            
+            Console.WriteLine("**************");
+            Console.WriteLine("Menu:");
+            Console.WriteLine("1 - New Game");
+            Console.WriteLine("2 - Load Game");
+            Console.WriteLine("3 - Options");
+            Console.WriteLine("4 - Quit");
+            Console.WriteLine("**************");
+
+            Console.Write("Enter your choice: ");
+            int choice; 
+            int.TryParse(Console.ReadLine(), out choice);
+            while (choice != 4)
+            {
+                // Handle the choice
+                Console.Write("Haha, this does nothing, try something else! ");
+                int.TryParse(Console.ReadLine(), out choice);
+            }
+            Console.WriteLine("Thanks for playing, exiting game!");
+        }
+    }
+}
