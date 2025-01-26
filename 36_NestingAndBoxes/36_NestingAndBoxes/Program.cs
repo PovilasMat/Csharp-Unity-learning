1f155b33255071971bc31a298a2bc261754b87cf
PovilasMat <povilas.matusevicius@gmail.com>
2025-01-26 18:26:14 +0100

    nesting exercise

---
+++ 36_NestingAndBoxes/36_NestingAndBoxes/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace _36_NestingAndBoxes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int width;
+            int height;
+
+            // get valid width
+            Console.Write("Enter box width (3-20): ");
+            width = int.Parse(Console.ReadLine());
+            while (width < 3 || width > 20)
+            {
+                Console.Write("Invalid width. Enter box width (3-20): ");
+                width = int.Parse(Console.ReadLine());
+            }
+
+            // get valid height
+            Console.Write("Enter box height (3-20): ");
+            height = int.Parse(Console.ReadLine());
+            while (height < 3 || height > 20)
+            {
+                Console.Write("Invalid height. Enter box height (3-20): ");
+                height = int.Parse(Console.ReadLine());
+            }
+            // draw box
+            DrawBox(width, height);
+        }
+        static void DrawBox(int width, int height)
+        {
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    if (i == 0 || i == height - 1 || j == 0 || j == width - 1)
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
