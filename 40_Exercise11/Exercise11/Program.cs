eddebf9e20a0d5dbd12313ad9439afab24d529d1
PovilasMat <povilas.matusevicius@gmail.com>
2025-01-29 23:39:53 +0100

    Cards list

---
+++ 40_Exercise11/Exercise11/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using ConsoleCards;
+
+namespace Exercise11
+{
+    /// <summary>
+    /// Exercise 11
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Exercise 11
+        /// </summary>
+        /// <param name="args">command-line args</param>
+        static void Main(string[] args)
+        {
+            Deck deck = new Deck();
+            Card[] cards = new Card[5];
+            deck.Shuffle();
+            Console.WriteLine("Your hand:");
+            for (int i = 0; i < 5; i++)
+            {
+                cards[i] = deck.TakeTopCard();
+                cards[i].FlipOver();
+                cards[i].Print();
+                //Console.WriteLine(cards[i].Rank + " of " + cards[i].Suit);
+            }
+        }
+    }
+}
