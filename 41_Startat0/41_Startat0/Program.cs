f0e6448a4db89f3c1b8fbc96fac442fb1bd9c3bf
PovilasMat <povilas.matusevicius@gmail.com>
2025-01-30 00:16:28 +0100

    unsafe code

---
+++ 41_Startat0/41_Startat0/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace _41_Startat0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] scores = { 100, 95, 90, 85, 80 };
+
+            // output array information
+            // ugly unsafe access to address of array element
+            // note use of unsafe block and compiled with unsafe code
+            unsafe
+            {
+                fixed (int* elementAddress = &scores[0])
+                {
+                    System.Console.WriteLine("First element at address: {0}", (int)elementAddress);
+                }
+                fixed (int* elementAddress = &scores[1])
+                {
+                    System.Console.WriteLine("Second element at address: {0}", (int)elementAddress);
+                }
+                fixed (int* elementAddress = &scores[2])
+                {
+                    System.Console.WriteLine("Third element at address: {0}", (int)elementAddress);
+                }
+                fixed (int* elementAddress = &scores[3])
+                {
+                    System.Console.WriteLine("Fourth element at address: {0}", (int)elementAddress);
+                }
+                fixed (int* elementAddress = &scores[4])
+                {
+                    System.Console.WriteLine("Fifth element at address: {0}", (int)elementAddress);
+                }
+            }
+        }
+    }
+}
