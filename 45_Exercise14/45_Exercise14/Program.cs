d4c5d980867527a8ff6cd51e946b6122b55045df
PovilasMat <povilas.matusevicius@gmail.com>
2025-02-02 21:32:54 +0100

    Exercise 14

---
+++ 45_Exercise14/45_Exercise14/Program.cs
@@ -0,0 +1,12 @@
+﻿using ConsoleCards;
+
+namespace _45_Exercise14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Card[] cards = new Card[5];
+        }
+    }
+}
