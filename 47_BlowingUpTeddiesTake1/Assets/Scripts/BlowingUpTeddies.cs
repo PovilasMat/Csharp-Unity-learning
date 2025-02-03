9c79d4e602453149f8ed30df18dfa3bd7bf5adce
PovilasMat <povilas.matusevicius@gmail.com>
2025-02-03 17:41:17 +0100

    blow up teedy by tag

---
+++ 47_BlowingUpTeddiesTake1/Assets/Scripts/BlowingUpTeddies.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Blows up teddies in response to player input
+/// </summary>
+public class BlowingUpTeddies : MonoBehaviour
+{
+	[SerializeField]
+	GameObject prefabExplosion;
+
+	[SerializeField]
+	Sprite greenTeddyBearSprite;
+	[SerializeField]
+	Sprite purpleTeddyBearSprite;
+	[SerializeField]
+	Sprite yellowTeddyBearSprite;
+
+	List<GameObject> gameObjects = new List<GameObject>();
+
+    /// <summary>
+    /// Update is called once per frame
+    /// </summary>
+    void Update()
+    {
+		// FindObjectsOfType is slow, so only call it
+		// if at least one of the axes has input
+		//if (Input.GetAxis("BlowUpYellowTeddies") > 0 ||
+		//	Input.GetAxis("BlowUpGreenTeddies") > 0 ||
+		//	Input.GetAxis("BlowUpPurpleTeddies") > 0)
+        //{
+		//	gameObjects.Clear();
+		//	gameObjects.AddRange(FindObjectsByType<GameObject>(FindObjectsSortMode.None));
+		//}
+
+		// blow up teddies as appropriate
+		if (Input.GetAxis("BlowUpYellowTeddies") > 0)
+        {
+			BlowUpTeddies(TeddyColor.Yellow);
+		}
+		if (Input.GetAxis("BlowUpGreenTeddies") > 0)
+        {
+			BlowUpTeddies(TeddyColor.Green);
+		}
+		if (Input.GetAxis("BlowUpPurpleTeddies") > 0)
+        {
+			BlowUpTeddies(TeddyColor.Purple);
+		}
+	}
+
+	/// <summary>
+	/// Blows up all the teddies of the given color
+	/// </summary>
+	/// <param name="color">color</param>
+	/// <param name="gameObjects">the game objects in the scene</param>
+	//void BlowUpTeddies(TeddyColor color, List<GameObject> gameObjects)
+	void BlowUpTeddies(TeddyColor color)
+	{
+		// blow up teddies of the given color
+		//for (int i = gameObjects.Count - 1; i >= 0; i--)
+		//{
+		//	SpriteRenderer spriteRenderer = gameObjects[i].GetComponent<SpriteRenderer>();
+		//	if (spriteRenderer != null)
+		//    {
+		//		Sprite sprite = spriteRenderer.sprite;
+		//		if ((color == TeddyColor.Green && sprite == greenTeddyBearSprite) ||
+		//			(color == TeddyColor.Purple && sprite == purpleTeddyBearSprite) ||
+		//			(color == TeddyColor.Yellow && sprite == yellowTeddyBearSprite))
+		//      {
+		//			BlowUpTeddy(gameObjects[i]);
+		//		}
+		//	 }
+		//}
+
+		gameObjects.Clear();
+		gameObjects.AddRange(GameObject.FindGameObjectsWithTag(color.ToString()));
+		for (int i = gameObjects.Count - 1; i >= 0; i--)
+		{
+			BlowUpTeddy(gameObjects[i]);
+		}
+	}
+    /// <summary>
+    /// Blows up the given teddy
+    /// </summary>
+    /// <param name="teddy">the teddy to blow up</param>
+    void BlowUpTeddy(GameObject teddy)
+    {
+		Instantiate(prefabExplosion, teddy.transform.position, Quaternion.identity);
+		Destroy(teddy);
+	}
+}
