8eb7ff73c365535a1161280995288c0cacf2e23e
PovilasMat <povilas.matusevicius@gmail.com>
2025-02-04 17:50:07 +0100

    Apple

---
+++ 52_Exercise3/52_Exercise3/Apple.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _52_Exercise3
+{
+    internal class Apple
+    {
+        #region Fields
+
+        float amountLeft;
+        bool organic;
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Gets the amount of apple left
+        /// </summary>
+        public float AmountLeft
+        {
+            get { return amountLeft; }
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether the apple is organic
+        /// </summary>
+        public Boolean Organic
+        {
+            get { return organic; }
+        }
+
+        #endregion
+
+        #region Methods
+
+        public void TakeBite(float size)
+        {
+            amountLeft -= size;
+        }
+
+        #endregion
+
+    }
+}
---
+++ 52_Exercise3/52_Exercise3/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace _52_Exercise3
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
