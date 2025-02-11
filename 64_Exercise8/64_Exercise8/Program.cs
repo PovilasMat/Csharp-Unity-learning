78b602f9f56038ee16ddb6d7b51dee568b834773
PovilasMat <povilas.matusevicius@gmail.com>
2025-02-12 00:47:47 +0100

    String data type

---
+++ 64_Exercise8/64_Exercise8/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _64_Exercise8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Print the month of your birth");
+            var month = Console.ReadLine();
+
+            Console.WriteLine("Print the day of your birth");
+            var day = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Is " + month + " " + day + "th your birthday? How do I know? Haha!");
+
+            Console.WriteLine("You will receive a special birthday promotion on " + month + " " + (day-1));
+        }
+    }
+}
