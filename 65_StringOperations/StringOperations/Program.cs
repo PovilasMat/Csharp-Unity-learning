75c1e37665cdaae9bb16add09ef3b932ac72b5cd
PovilasMat <povilas.matusevicius@gmail.com>
2025-02-12 01:00:48 +0100

    StringOperations

---
+++ 65_StringOperations/StringOperations/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace StringOperations
+{
+    /// <summary>
+    /// String Operations lecture code
+    /// </summary>
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // read in csv string
+            Console.Write("Enter name and percent (name,percent): ");
+            string csvString = Console.ReadLine();
+
+            // find comma location
+            int commaLocation = csvString.IndexOf(',');
+
+            // validate input
+            if (commaLocation == -1)
+            {
+                Console.WriteLine("Error: Input does not contain a comma.");
+                return;
+            }
+
+            // extract name and percent
+            string name = csvString.Substring(0, commaLocation);
+            string percentString = csvString.Substring(commaLocation + 1);
+
+            if (float.TryParse(percentString, System.Globalization.NumberStyles.Float, System.Globalization.CultureInfo.InvariantCulture, out float percent)) // I need to use InvariantCulture to make sure that the decimal separator is a dot
+            {
+                // print name and percent
+                Console.WriteLine("Name: " + name);
+                Console.WriteLine("Percent: " + percent);
+            }
+            else
+            {
+                Console.WriteLine("Error: Percent is not in a correct format.");
+            }
+        }
+    }
+}
