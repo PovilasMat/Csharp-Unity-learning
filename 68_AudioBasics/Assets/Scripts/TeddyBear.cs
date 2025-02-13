866f3f36f6cb95c67dda72c4b747333cf2ae7db1
PovilasMat <povilas.matusevicius@gmail.com>
2025-02-14 00:19:24 +0100

    AudioBasics module

---
+++ 68_AudioBasics/Assets/Scripts/TeddyBear.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// A teddy bear
+/// </summary>
+public class TeddyBear : MonoBehaviour
+{
+    /// <summary>
+	/// Start is called before the first frame update
+	/// </summary>	
+    void Start()
+    {
+
+    }
+
+    /// <summary>
+    /// Play sound when mouse enters collider
+    /// </summary>
+    void OnMouseEnter()
+    {
+
+    }
+}
