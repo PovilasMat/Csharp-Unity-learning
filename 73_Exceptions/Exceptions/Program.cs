afb30d267daafab0010b45cbf193a8c9ed643bfc
PovilasMat <povilas.matusevicius@gmail.com>
2025-02-22 01:17:05 +0100

    catching exceptions

---
+++ 73_Exceptions/Exceptions/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Exceptions
+{
+    /// <summary>
+    /// Exceptions lectures code
+    /// </summary>
+    internal class Program
+    {
+        /// <summary>
+        /// Demonstrates exceptions and exception handling
+        /// </summary>
+        /// <param name="args">command-line arguments</param>
+        static void Main(string[] args)
+        {
+            try
+            {
+                // prompt for and get a short
+                Console.Write("Enter a whole number (-32,768 to 32,767): ");
+                short input = short.Parse(Console.ReadLine());
+            }
+            catch (FormatException fe) 
+            {
+                Console.WriteLine("That's not a whole number!");
+            }
+            catch (OverflowException oe)
+            {
+                Console.WriteLine("That number is out of range!");
+            }
+            finally
+            {
+                Console.WriteLine("All done");
+            }
+        }
+    }
+}
